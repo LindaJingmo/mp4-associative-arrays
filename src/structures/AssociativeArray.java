c8e29bb40b303b18988315f756b2d2253f03e259
Linda Jing <lindajing@LindadeMacBook-Pro.local>
2024-02-27 11:11:02 -0600

    update mp4

--- src/structures/AssociativeArray.java
+++ src/structures/AssociativeArray.java
@@ -2,6 +2,8 @@ package structures;
 
 import static java.lang.reflect.Array.newInstance;
 
+import java.util.NoSuchElementException;
+
 /**
  * A basic implementation of Associative Arrays with keys of type K
  * and values of type V. Associative Arrays store key/value pairs
@@ -56,16 +58,55 @@ public class AssociativeArray<K, V> {
   /**
    * Create a copy of this AssociativeArray.
    */
+  @SuppressWarnings("unchecked")
+  @Override
   public AssociativeArray<K, V> clone() {
-    return null; // STUB
-  } // clone()
+    AssociativeArray newArray= new AssociativeArray<>();
+    newArray.size=this.size;
+    for(int i =0;i<this.size;i++){
+      if(this.pairs[i]!=null){
+          newArray.pairs[i] = new KVPair<K,V>(this.pairs[i].key,this.pairs[i].value);
+    }
+    }
+    return newArray;
+
+
+    /* 
+      try {
+          AssociativeArray<K, V> clone = (AssociativeArray<K, V>) super.clone();
+          clone.pairs = new KVPair[this.pairs.length];
+          for (int i = 0; i < this.pairs.length; i++) {
+              // Assuming KVPair has a proper copy constructor or a clone method
+              clone.pairs[i] = (this.pairs[i] != null) ? this.pairs[i].clone() : null;
+          }
+          return clone;
+      } catch (CloneNotSupportedException e) {
+          throw new AssertionError(); // Can never happen if the object is Cloneable
+      }
+
+      */
+  }
 
   /**
    * Convert the array to a string.
    */
+  @Override
   public String toString() {
-    return "{}"; // STUB
-  } // toString()
+      if (this.size == 0) {
+          return "{}";
+      }
+  
+      StringBuilder sb = new StringBuilder();
+      sb.append("{ ");
+      for (int i = 0; i < this.size; i++) {
+          sb.append(this.pairs[i].key).append(": ").append(this.pairs[i].value);
+          if (i < this.size - 1) {
+              sb.append(", ");
+          }
+      }
+      sb.append(" }");
+      return sb.toString();
+  }
 
   // +----------------+----------------------------------------------
   // | Public Methods |
@@ -75,9 +116,31 @@ public class AssociativeArray<K, V> {
    * Set the value associated with key to value. Future calls to
    * get(key) will return value.
    */
+/* 
+   public void set(K key, V value){
+    map.put(key, value);
+}
+*/
+   
   public void set(K key, V value) throws NullKeyException {
-    // STUB
+    if (key == null) {
+      throw new NullKeyException("Key cannot be null");
+  }
+    for(int i =0;i<this.size;i++){
+      if(this.pairs[i].key.equals(key)){
+        this.pairs[i].value=value;
+        return;
+      }
+    }
+    if (this.size == this.pairs.length) {
+      this.expand();
+  }
+
+  this.pairs[this.size] = new KVPair<>(key, value);
+  this.size++;
+
   } // set(K,V)
+
 
   /**
    * Get the value associated with key.
@@ -87,15 +150,27 @@ public class AssociativeArray<K, V> {
    *                              appear in the associative array.
    */
   public V get(K key) throws KeyNotFoundException {
-    return null; // STUB
-  } // get(K)
+    for(int i =0;i<this.size;i++){
+      if(this.pairs[i].key.equals(key)){
+        return this.pairs[i].value;
+    }
+  }
+     throw new KeyNotFoundException ("Key not found: " + key);
+    
+    } // get(K)
 
   /**
    * Determine if key appears in the associative array. Should
    * return false for the null key.
    */
+  //@SuppressWarnings("unused")
   public boolean hasKey(K key) {
-    return false; // STUB
+    for(int i =0;i<this.size;i++){
+      if(this.pairs[i].key.equals(key)){
+        return true;
+      } 
+    }
+    return false;
   } // hasKey(K)
 
   /**
@@ -104,8 +179,17 @@ public class AssociativeArray<K, V> {
    * in the associative array, does nothing.
    */
   public void remove(K key) {
-    // STUB
-  } // remove(K)
+    for (int i = 0; i < this.size; i++) {
+        if (this.pairs[i].key.equals(key)) {
+            for (int j = i; j < this.size - 1; j++) {
+                this.pairs[j] = this.pairs[j + 1];
+            }
+            this.size--;
+            return;
+        }
+    }
+}
+
 
   /**
    * Determine how many key/value pairs are in the associative array.
@@ -114,6 +198,7 @@ public class AssociativeArray<K, V> {
     return this.size;
   } // size()
 
+  
   // +-----------------+---------------------------------------------
   // | Private Methods |
   // +-----------------+
